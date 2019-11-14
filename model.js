8fc1f273acd7a8e4f421f9bf0d6a740f3426fe6c
LiteMasteralex <alexlara_97@hotmail.com>
2019-11-14 15:43:54 -0600

    Finished courses model CRUD

--- model.js
+++ model.js
@@ -13,7 +13,7 @@ let historySchema = mongoose.Schema({
 });
 
 let courseSchema = mongoose.Schema({
-	name: { type : String },
+	name: { type : String, require : true },
 	allotedTime: { type : Number },
 	spentTime: { type : Number},
 	tasks: [taskSchema]
@@ -26,8 +26,8 @@ let pomodoroSchema = mongoose.Schema({
 });
 
 let usersSchema = mongoose.Schema({
-	email: { type : String },
-	password: { type : String },
+	email: { type : String, require : true },
+	password: { type : String, require : true },
 	courses: [courseSchema],
 	history: historySchema,
 	pomodoro: pomodoroSchema
@@ -35,74 +35,16 @@ let usersSchema = mongoose.Schema({
 
 let User = mongoose.model( 'User', usersSchema );
 
-let StudentList = {
-	getAll : function() {
-		return Student.find().then(function(students) {
-			return students;
-		}).catch(function(error) {
-			throw Error(error);
-		});
-	},
-	getOne : function(studentId) {
-		return Student.find({id: studentId}).then(function(student) {
-			return student;
-		}).catch(function(error) {
-			throw Error(error);
-		});
-	},
-	post : function(newStudent) {
-		return Student.find(newStudent).then(function(studentList) {
-			if (studentList.length == 0) {
-				return Student.create(newStudent).then(function(student) {
-					return student;
-				}).catch(function(error) {
-					throw Error(error);
-				});
-			}
-			return 409;
-		}).catch(function(error) {
-			throw Error(error);
-		});
-	},
-	delete : function(studentId) {
-		return Student.findOneAndRemove({id: studentId}).then(function(student) {
-			return student
-		}).catch(function(error) {
-			throw Error(error);
-		});
-	},
-	put : function( updatedStudent ){
-		return StudentList.getByID( updatedStudent.id )
-			.then( student => {
-				if ( student ){
-					return Student.findOneAndUpdate( {id : student.id}, {$set : updatedStudent}, {new : true})
-						.then( newStudent => {
-							return newStudent;
-						})
-						.catch(error => {
-							throw Error(error);
-						});
-				}
-				else{
-					throw Error( "404" );
-				}
-			})
-			.catch( error => {
-				throw Error(error);
-			});
-	}
-}
-
 let UserList = {
-	get : function(user) {
-		return User.find({username: user.username}).then(function(foundUser) {
+	getByEmail : function(email) {
+		return User.find({email: email}).then(foundUser => {
 			return foundUser;
-		}).catch(function(error) {
+		}).catch(error => {
 			throw Error(error);
 		});
 	},
-	post : function(newUser) {
-		return User.find({username: newUser.username}).then(userList => {
+	postUser : function(newUser) {
+		return User.find({email: newUser.email}).then(userList => {
 			if (userList.length == 0) {
 				return User.create(newUser).then(user => {
 					return user;
@@ -110,11 +52,33 @@ let UserList = {
 					throw Error(error);
 				});
 			}
-			throw Error( " 409 ");
+			return 409;
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+	postCourse : function(email, newCourse) {
+		return User.findOneAndUpdate({email: email}, {$push: {courses: newCourse}}, { new: true }).then( user => {
+			return user.course[user.course.length - 1];
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+	putCourse : function(email, course) {
+		return User.findOneAndUpdate({email: email, course.name: course.name}, {$set: {course.$: course}}, { new: true }).then( user => {
+			return user.course //We could filter out and return only the course we updated.
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+	deleteCourse : function(email, course) {
+		return User.findOneAndUpdate({email: email}, {$pullAll: { courses: [course]}}).then( => {
+			return 202;
 		}).catch(error => {
 			throw Error(error);
 		});
 	}
+
 }
 
 module.exports = { StudentList, UserList };
