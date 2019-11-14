225471e26ce8f881e74339126fb5efae85f21b79
LiteMasteralex <alexlara_97@hotmail.com>
2019-11-14 16:07:40 -0600

    Added create user endpoint

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 let express = require('express');
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
-
+let mongoose = require('mongoose');
+let { UserList } = require('./model');
 // Setup
 let app = express();
 let jsonParser = bodyParser.json();
@@ -11,10 +12,58 @@ app.use(express.static('public'));
 app.use(morgan('combined'));
 
 // API
+app.post('/api/register', jsonParser, (req, res) => {
+	let {email, password} = req.body;
+	UserList.postUser({email, password}).then(user => {
+		console.log(user);
+		return res.status(200).json(user);
+	}).catch(err => {
+		console.log(err);
+		return res.status(500).json(err);
+	});
+})
 
-// Running and closing the server
-app.listen("8080", () => {
-    console.log("App is running on port 8080");
+let server;
+
+function runServer(port, databaseUrl) {
+	return new Promise( function(resolve, reject) {
+		mongoose.connect(databaseUrl, function(error) {
+			if (error) {
+				return reject(error);
+			}
+			else {
+				server = app.listen(port, function() {
+					console.log('You have entered a cursed land ' + port);
+					resolve();
+				}).on('error', function(error) {
+					mongoose.disconnect();
+					return reject(error);
+				});
+			}
+		});
+	});
+};
+
+runServer("8080", "mongodb://localhost/telosTest").catch(function(error) {
+	console.log(error);
 });
+
+function closeServer(){
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing the server');
+			server.close( err => {
+				if (err){
+					return reject(err);
+				}
+				else{
+					resolve();
+				}
+			});
+		});
+	});
+}
+
+module.exports = { app, runServer, closeServer };
 
 // Exports
