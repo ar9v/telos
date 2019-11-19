df206ecb98d1cf3f414d823db5b5c5037498ce64
Ricardo Vela <aricav96@gmail.com>
2019-11-18 20:31:45 -0600

    Add user context to dashboard. Code to render time
    - Using MIN m SEC s convention for now to avoid the stringly typed
      ugliness of dealing with drawing the timer as 00:00

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,63 @@
-// We must obtain a user's information to populate the dashboard
-// $.ajax({
-//     url: '/api/User',
-//     data: { email: "aricav96@gmail.com" },
-//     dataType: "json",
-//     method: "GET",
-//     success: function(response) {
+var userContext = {
+    email: "",
+    courses: [],
+    history: [],
+    pomodoro: {
+        workLength: 0,
+        breakLength: 0,
+        longBreakLength: 0,
+    },
+    timer: {
+        mins: 0,
+        sec: 0
+    },
+    pomodoroCount: 0
+};
 
-//     }
-// });
+// We must obtain a user's information to populate the dashboard
+$.ajax({
+    url: '/api/User',
+    data: { email: "aricav96@gmail.com" },
+    dataType: "json",
+    method: "GET",
+    success: function(response) {
+        // Response should be the Mongo array
+        // hence the index
+        setUserContext(response[0]);
+        populate();
+    },
+    error: function(err) { console.log(err) }
+});
+
+function setUserContext(mongoUser) {
+    userContext.email = mongoUser.email;
+    mongoUser.courses.forEach(course => userContext.courses.push(course));
+    mongoUser.history.forEach(hist => userContext.history.push(hist));
+    userContext.pomodoro.workLength = mongoUser.pomodoro.workLength;
+    userContext.pomodoro.breakLength = mongoUser.pomodoro.breakLength;
+    userContext.pomodoro.longBreakLength = mongoUser.pomodoro.longBreakLength;
+}
+
+function populate() {
+    setTimer();
+    // loadConfig();
+    // loadCourses();
+    // loadHistory();
+}
+
+function setTimer() {
+    let cycleState = userContext.pomodoroCount % 8;
+
+    if(cycleState == 7)
+        userContext.timer.mins = userContext.pomodoro.longBreakLength;
+    else if(cycleState % 2 == 0)
+        userContext.timer.mins = userContext.pomodoro.workLength;
+    else
+        userContext.timer.mins = userContext.pomodoro.breakLength;
+
+    $("#timer").text(`${userContext.timer.mins}m ${userContext.timer.sec}s`)
+}
+
+// function loadConfig() {
+
+// }
